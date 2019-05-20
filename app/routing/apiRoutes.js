e1fbe32b4ea76fcdf891117de163d6e140f6fcd4
TesS <terena.blasberg@gmail.com>
2019-05-20 14:29:47 -0400

    updates on api routes

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,5 +1,17 @@
-var friendsData = require("../data/friends");
+var friends = require("../data/friends");
 
-app.get("/api/friends", function(req, res){
-    res.json(friendsData);
-});
+
+
+module.exports = function (app){
+    app.get("/api/friends", function(req, res){
+        res.json(friends);
+    });
+
+    app.post("/api/friends", function(req, res){
+        
+    })
+
+
+
+
+}
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require("express");
 var app = express();
+var bodyParser = require('body-parser');
 
 
 var PORT = process.env.PORT || 3000;
@@ -9,7 +10,17 @@ app.use(express.urlencoded({
 }));
 app.use(express.json());
 
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+ 
+// parse application/json
+app.use(bodyParser.json())
+ 
+
+
 require("./app/routing/htmlRoutes")(app);
+require("./app/routing/apiRoutes")(app);
 
 
 app.listen(PORT, function(){
